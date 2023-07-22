3f62206ff6664e5f119e6702735540ff54e0fcc6
TuftedDeer <36223345+tufteddeer@users.noreply.github.com>
2023-07-22 09:16:20 +0200

    Init

---
+++ flake.nix
@@ -0,0 +1,53 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nix-community/naersk";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = { self, flake-utils, naersk, nixpkgs, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = (import nixpkgs) {
+          inherit system overlays;
+        };
+
+        toolchain = pkgs.rust-bin.selectLatestNightlyWith
+          (toolchain: toolchain.default.override {
+            extensions = [ "rust-src" ];
+          });
+
+        naersk' = pkgs.callPackage naersk {
+          cargo = toolchain;
+          rustc = toolchain;
+        };
+
+        buildInputs = with pkgs; [
+        ];
+      in
+      rec {
+        # For `nix build` & `nix run`:
+        defaultPackage = naersk'.buildPackage
+          {
+            src = ./.;
+            nativeBuildInputs = with pkgs; [
+            ];
+
+
+
+          };
+
+        # For `nix develop` (optional, can be skipped):
+        devShell = pkgs.mkShell
+          {
+            nativeBuildInputs = with pkgs; [
+              toolchain
+            ];
+
+
+          };
+      }
+    );
+}
