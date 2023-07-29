b41e6c1f6cd6d5e25cb9efdbdf34591765e7606b
TuftedDeer <36223345+tufteddeer@users.noreply.github.com>
2023-07-29 13:07:29 +0200

    Restrict systemd unit

--- modules/audiothek-feed/default.nix
+++ modules/audiothek-feed/default.nix
@@ -1,25 +1,17 @@
 flake: { config, lib, pkgs, ... }:
 
 let
-  inherit (builtins) toJSON removeAttrs;
-  inherit (lib) filterAttrs types mkEnableOption mkOption mkRenamedOptionModule;
-  inherit (lib.trivial) pipe;
-
-  inherit (flake.packages.${pkgs.stdenv.hostPlatform.system}) defaultPackage;
+  inherit (lib)  mkEnableOption;
 
   cfg = config.services.audiothekfeed;
 
   package = flake.defaultPackage.${pkgs.stdenv.hostPlatform.system};
 in
 {
-  # imports = [
-  #   (mkRenamedOptionModule [ "services" "foundryvtt" "hostname" ] [ "services" "foundryvtt" "hostName" ])
-  # ];
 
   options = {
     services.audiothekfeed = {
       enable = mkEnableOption ''
-        Foundry Virtual Tabletop: A standalone application for online tabletop role-playing games.
       '';
       };
       };
@@ -35,10 +27,9 @@ in
 
     systemd.services.audiothekfeed = {
       description = "Audiothek Atom feed provider";
-      # documentation = [ "https://foundryvtt.com/kb/" ];
 
       after = [ "network-online.target" ];
-      # wantedBy = [ "multi-user.target" ];
+      wantedBy = [ "multi-user.target" ];
 
       serviceConfig = {
         User = "audiothekfeed";
@@ -49,40 +40,35 @@ in
         StateDirectoryMode = "0750";
 
         # Hardening
-        # CapabilityBoundingSet = [ "AF_NETLINK" "AF_INET" "AF_INET6" ];
+        apabilityBoundingSet = [ "AF_NETLINK" "AF_INET" "AF_INET6" ];
         # DeviceAllow = [ "/dev/stdin r" ];
-        # DevicePolicy = "strict";
-        # IPAddressAllow = "localhost";
-        # LockPersonality = true;
-        # # MemoryDenyWriteExecute = true;
-        # NoNewPrivileges = true;
-        # PrivateDevices = true;
-        # PrivateTmp = true;
-        # PrivateUsers = true;
-        # ProtectClock = true;
-        # ProtectControlGroups = true;
-        # ProtectHome = true;
-        # ProtectHostname = true;
-        # ProtectKernelLogs = true;
-        # ProtectKernelModules = true;
-        # ProtectKernelTunables = true;
-        # ProtectSystem = "strict";
+        DevicePolicy = "strict";
+        IPAddressAllow = "localhost";
+        LockPersonality = true;
+        MemoryDenyWriteExecute = true;
+        NoNewPrivileges = true;
+        fmemoryPrivateDevices = true;
+        PrivateTmp = true;
+        PrivateUsers = true;
+        ProtectClock = true;
+        ProtectControlGroups = true;
+        ProtectHome = true;
+        ProtectHostname = true;
+        ProtectKernelLogs = true;
+        ProtectKernelModules = true;
+        ProtectKernelTunables = true;
+        ProtectSystem = "strict";
         # ReadOnlyPaths = [ "/" ];
-        # RemoveIPC = true;
-        # RestrictAddressFamilies = [ "AF_NETLINK" "AF_INET" "AF_INET6" ];
-        # RestrictNamespaces = true;
-        # RestrictRealtime = true;
-        # RestrictSUIDSGID = true;
-        # SystemCallArchitectures = "native";
-        # SystemCallFilter = [ "@system-service" "~@privileged" "~@resources" "@pkey" ];
+        RemoveIPC = true;
+        RestrictAddressFamilies = [ "AF_NETLINK" "AF_INET" "AF_INET6" ];
+        RestrictNamespaces = true;
+        RestrictRealtime = true;
+        RestrictSUIDSGID = true;
+        SystemCallArchitectures = "native";
+        SystemCallFilter = [ "@system-service" "~@privileged" "~@resources" "@pkey" ];
         # UMask = "0027";
       };
 
-      # preStart = ''
-      #   installedConfigFile="${config.services.foundryvtt.dataDir}/Config/options.json"
-      #   install -d -m750 ${config.services.foundryvtt.dataDir}/Config
-      #   rm -f "$installedConfigFile" && install -m640 ${configFile} "$installedConfigFile"
-      # '';
     };
   };
 }
